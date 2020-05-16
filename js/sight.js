fb23fdd9a6a35a954900018e585f33e2ae3ab5e6
sumit <sumit.choudhary@eclinicalworks.com>
2020-05-16 12:04:25 -0400

    initial

--- js/sight.js
+++ js/sight.js
@@ -3,6 +3,8 @@ class SVGElement {
     constructor(type) {
         this.type = type;
         this.namespace = 'http://www.w3.org/2000/svg';
+        this.node = document.createElementNS(this.namespace,type)
+        return this
 
     }
 }
