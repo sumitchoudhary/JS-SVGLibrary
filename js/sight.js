030ff0595d0c6caa36285cdcac3300e301da768f
sumit <sumit.choudhary@eclinicalworks.com>
2020-05-16 15:31:19 -0400

    initial

--- js/sight.js
+++ js/sight.js
@@ -10,19 +10,23 @@ class SVGElement {
         for (const [key, value] of Object.entries(attrs)) {
            this.node.setAttributeNS(null,key,value)
         }
-        return this;
-    }
+        return this;}
 
     append(element){
       const parent = (typeof element === 'string') ? document.querySelector(element): element.node;
       parent.appendChild(this.node)
         return this}
-
-
 }
 
 class Sight {
     constructor(selector, width, height) {
-        this.svg = new SVGElement('svg').attr({viewbox : `0 0 ${width} ${height}`}).append(selector)
+        this.svg = new SVGElement('svg').attr({viewbox: `0 0 ${width} ${height}`}).append(selector)
     }
-}
+
+        draw(type, attrs) {
+            return new SVGElement(type).attr(attrs).append(this.svg);
+        }
+
+
+    }
+
